51bf50ce3e295163ff50dce472d9821dc8aa171a
Kv552 <105648070+Kv552@users.noreply.github.com>
2023-11-07 19:40:08 -0300

    Delete trabgb-main/kv directory

--- trabgb-main/kv/Instancia.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require "Conta.php";
-require "Cliente.php";
-
-$conta = new Conta(57349, "kv", 0.00);
-$cliente = new Cliente("kevin", "000.000.000.00", "10/10/1000", $conta);
-
-echo $cliente->retornarConta()->retornarNome();
